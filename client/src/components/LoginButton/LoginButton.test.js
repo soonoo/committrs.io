78f79cac287859e1a07ec01fa73fabf8729d1fa6
soonoo <qpseh2m7@gmail.com>
2019-01-28 01:56:46 +0900

    add LoginButton component and test

---
+++ client/src/components/LoginButton/LoginButton.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import LoginButton from './';
+
+let wrap;
+describe('LoginButton.js', () => {
+  it('renders without crashing', () => {
+    wrap = shallow(<LoginButton />);
+  });
+
+  it('redirects to github.com', () => {
+    const url = `https://github.com/login/oauth/authorize?client_id=435deb42a14081c0a9bf&redirect_uri=https://committrs.io/login`;
+
+    expect(wrap.find('a').props()).toHaveProperty('href', url);
+  });
+
+  it('has inner text "GitHub으로 시작하기"', () => {
+    expect(wrap.find('a').text()).toEqual('GitHub으로 시작하기');
+  });
+});
+
---
+++ client/src/components/LoginButton/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoginButton = () => {
+  return (
+    <a
+      href='https://github.com/login/oauth/authorize?client_id=435deb42a14081c0a9bf&redirect_uri=https://committrs.io/login'
+    >
+      GitHub으로 시작하기
+    </a>
+  );
+};
+
+export default LoginButton;
+
