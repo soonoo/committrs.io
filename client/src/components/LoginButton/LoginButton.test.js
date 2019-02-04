a46fa5e9dc277c4c18a179f6329e3637577a5df1
soonoo <qpseh2m7@gmail.com>
2019-02-05 00:20:20 +0900

    change testing library from enzyme to react-testing-library
    react-testing-library has better support for react Context API.

--- client/src/components/LoginButton/LoginButton.test.js
+++ client/src/components/LoginButton/LoginButton.test.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import LoginButton from './';
 import { LocaleContext } from 'context';
+import { render } from 'react-testing-library';
 
 let wrap;
 describe('LoginButton.js', () => {
   it('renders without crashing', () => {
-    wrap = shallow(<LoginButton />, { context: { locale: 'en' } });
+    wrap = render(
+      <LocaleContext.Provider value='en'>
+        <LoginButton />
+      </LocaleContext.Provider>
+    );
   });
 
   it('redirects to github.com', () => {
     const url = `https://github.com/login/oauth/authorize?client_id=435deb42a14081c0a9bf&redirect_uri=https://committrs.io/login`;
 
-    //expect(wrap.renderProp('children')).toHaveProperty('href', url);
+    expect(wrap.getByText(/^Getting/).getAttribute('href')).toEqual(url)
   });
 });
 
