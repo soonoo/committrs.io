de99c336f9e57d711bff7ed24961aa0cf972e558
soonoo <qpseh2m7@gmail.com>
2019-02-14 23:39:54 +0900

    add utility git command named sinceLog

--- batch/utils/index.js
+++ batch/utils/index.js
@@ -11,5 +11,8 @@ module.exports = {
   pull: async () => execPromise(`git pull --all`),
   clone: async (path) => execPromise(`git clone --no-checkout https://github.com/${path} repos/${path}`),
   log: async (username) => execPromise(`git log --author='${username}' --all --stat --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`),
+
+  // TODO: --since flag should be added to command
+  logSince: async (username, since) => execPromise(`git log --author='${username}' --all --stat --since=${since} --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`),
 };
 
