af9466c89826e6fa193245ce79fe2463e522c8fa
soonoo <qpseh2m7@gmail.com>
2019-02-14 23:41:01 +0900

    [batch] add structure of syncUserHandler.js

--- batch/subscriber/handler/syncUserHandler.js
+++ batch/subscriber/handler/syncUserHandler.js
@@ -1,5 +1,24 @@
-const syncUserHandler = (message) => {
+const { pull, logSince, log } = require('../../utils');
 
+const syncUserHandler = async (message) => {
+  // TODO: get list of not updated repos from database
+  const repos = [];
+
+  for(repo of repos) {
+    process.cwd(repo.path);
+
+    await pull();
+
+    // TODO: get list of users who has/forked `repo`
+    const users = [];
+
+    for(user of user) {
+      // TODO: get list of commits after
+      const commits = [];
+
+      // TODO: add commits to database
+    }
+  }
 };
 
 module.exports = syncUserHandler;
