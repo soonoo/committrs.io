a3d81bd4ec27149675de432e9e4aa0fa3dd8a1a3
soonoo <qpseh2m7@gmail.com>
2019-02-17 18:14:33 +0900

    [batch] add splitCommits util function

--- batch/utils/index.js
+++ batch/utils/index.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
 const execPromise = (command) => new Promise((resolve, reject) => {
   exec(command, (error, stdout, stderr) => {
@@ -7,29 +7,28 @@ const execPromise = (command) => new Promise((resolve, reject) => {
   });
 });
 
-module.exports = {
-  pull: async () => execPromise(`git pull --all`),
-  clone: async (path) => execPromise(`git clone --no-checkout https://github.com/${path} repos/${path}`),
-  log: async (username) => execPromise(`git log --author='${username}' --all --stat --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`),
 
-  logSince: async (username, since) => execPromise(`git log --author='${username}' --all --stat --since=${since} --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`),
+const pull = async () => execPromise(`git pull --all`);
+const clone = async (path) => execPromise(`git clone --no-checkout https://github.com/${path} repos/${path}`);
+const log = async (username) => execPromise(`git log --author='${username}' --all --stat --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`);
 
-  splitCommits: (gitLog) =>  {
-    const re = /---committrs\/(?:sep|hash|date|subject|body|files_changed)---\n/g;
-    const commits = gitLog.split(/---committrs\/sep---/g);
+const logSince = async (username, since) => execPromise(`git log --author='${username}' --all --stat --since=${since} --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`);
 
-    const commitList = commits.map((commit) => {
-      const commitData = commit.split(/---committrs\/(?:hash|date|subject|body|files_changed)---\n/g).slice(1);
-      return {
-        hash: commitData[0],
-        date: commitData[1],
-        subject: commitData[2],
-        body: commitData[3],
-        filesChanged: commitData[4],
-      };
-    });
+const splitCommits = (gitLog) => {
+  const re = /---committrs\/(?:sep|hash|date|subject|body|files_changed)---\n/g;
+  const commits = gitLog.split(/---committrs\/sep---/g).slice(1);
 
-    return commitList;
-  }
-};
+  const commitList = commits.map((commit) => {
+    const commitData = commit.split(/---committrs\/(?:hash|date|subject|body|files_changed)---\n/g).slice(1);
+    return {
+      hash: commitData[0],
+      date: commitData[1],
+      subject: commitData[2],
+      body: commitData[3],
+      filesChanged: commitData[4],
+    };
+  });
+
+  return commitList;
+}
 
