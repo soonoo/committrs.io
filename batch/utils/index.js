8636dda13b5751b09f1f63065d489e058fef836d
soonoo <qpseh2m7@gmail.com>
2019-02-17 18:14:33 +0900

    [batch] add splitCommits util function

--- batch/utils/index.js
+++ batch/utils/index.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 const execPromise = (command) => new Promise((resolve, reject) => {
   exec(command, (error, stdout, stderr) => {
     if (error) return reject(error);
-    resolve(stdout.trim());
+    return resolve(stdout.trim());
   });
 });
 
@@ -12,7 +12,24 @@ module.exports = {
   clone: async (path) => execPromise(`git clone --no-checkout https://github.com/${path} repos/${path}`),
   log: async (username) => execPromise(`git log --author='${username}' --all --stat --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`),
 
-  // TODO: --since flag should be added to command
   logSince: async (username, since) => execPromise(`git log --author='${username}' --all --stat --since=${since} --pretty=format:'---committrs/sep---%n---committrs/hash---%n%H%n---committrs/date---%n%aI%n---committrs/subject---%n%s%n---committrs/body---%n%b---committrs/files_changed---'`),
+
+  splitCommits: (gitLog) =>  {
+    const re = /---committrs\/(?:sep|hash|date|subject|body|files_changed)---\n/g;
+    const commits = gitLog.split(/---committrs\/sep---/g);
+
+    const commitList = commits.map((commit) => {
+      const commitData = commit.split(/---committrs\/(?:hash|date|subject|body|files_changed)---\n/g).slice(1);
+      return {
+        hash: commitData[0],
+        date: commitData[1],
+        subject: commitData[2],
+        body: commitData[3],
+        filesChanged: commitData[4],
+      };
+    });
+
+    return commitList;
+  }
 };
 
