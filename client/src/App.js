27ab926871aa213fa584dacb51599f2d5cabd216
soonoo <qpseh2m7@gmail.com>
2019-02-19 00:37:12 +0900

    [client] check first if browser locale is ko-KR

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const store = createStore(reducers);
 
 class App extends Component {
   state = {
-    locale: window.navigator.language === 'en-US' ? 'en' : 'ko',
+    locale: window.navigator.language === 'ko-KR' ? 'ko' : 'en',
   }
 
   render() {
