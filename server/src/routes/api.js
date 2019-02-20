fa6f2451f9d06a02597445170cc7e9a2c521e98c
soonoo <qpseh2m7@gmail.com>
2019-02-20 19:15:22 +0900

    [server] add route for /repos/:username
    request to /repose/:username returns total count of commits per repos

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,16 +1,35 @@
+import User from '../../db/model/User';
+import Repo from '../../db/model/Repo';
+import Commit from '../../db/model/Commit';
+import sync from '../../db/sync';
 import Router from 'koa-router';
-const router = new Router();
+import sequelize from '../../db/index';
 
+const router = new Router();
 // TODO: urls have to be refined
 
 // get commits of a user for a repo
 router.get('/commits/:username/:owner/:repo', async (ctx) => {
-
 });
 
 // get list of repos for a user
 router.get('/repos/:username', async (ctx) => {
+  await sync();
+  const { username } = ctx.params;
+  const query = `
+    SELECT repoId, COUNT(commitId) AS commitsCount FROM users JOIN
+    (SELECT commits.id AS commitId, repos.id AS repoId, commits.userId AS userId
+    FROM commits JOIN repos ON commits.repoId = repos.id) AS bb 
+    ON users.id = bb.userId WHERE users.name = $username GROUP BY repoId;
+  `;
+  const options = {
+    type: sequelize.QueryTypes.SELECT,
+    bind: {
+      username,
+    },
+  };
 
+  ctx.body = await sequelize.query(query, options);
 });
 
 export default router;
