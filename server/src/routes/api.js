5d7a6171adf93682c09f557d90b450e3d4cb37eb
soonoo <qpseh2m7@gmail.com>
2019-02-22 22:50:17 +0900

    [server] add routes
    /repos/:userId - get commit count per repo of a user
    /commits/:repoId/:userId - get commits of user's repo

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -6,30 +6,56 @@ import Router from 'koa-router';
 import sequelize from '../../db/index';
 
 const router = new Router();
-// TODO: urls have to be refined
 
-// get commits of a user for a repo
-router.get('/commits/:username/:owner/:repo', async (ctx) => {
+router.get('/commits/:userId/:repoId', async (ctx) => {
+  await sync();
+  const { userId, repoId } = ctx.params;
+  const { Op } = sequelize;
+
+  ctx.body = await Repo.findOne({
+    include:[{
+      model: Commit,
+      attributes: {
+        exclude: [ 'createdAt', 'updatedAt', 'userId', 'repoId', ],
+      },
+      where: {
+        [Op.and]: [
+          { repoId },
+          { userId },
+        ],
+      },
+    }],
+  });
 });
 
-// get list of repos for a user
-router.get('/repos/:username', async (ctx) => {
+router.get('/repos/:userId', async (ctx) => {
   await sync();
-  const { username } = ctx.params;
-  const query = `
-    SELECT repoId, COUNT(commitId) AS commitsCount FROM users JOIN
-    (SELECT commits.id AS commitId, repos.id AS repoId, commits.userId AS userId
-    FROM commits JOIN repos ON commits.repoId = repos.id) AS bb 
-    ON users.id = bb.userId WHERE users.name = $username GROUP BY repoId;
-  `;
-  const options = {
-    type: sequelize.QueryTypes.SELECT,
-    bind: {
-      username,
-    },
-  };
+  const { userId } = ctx.params;
 
-  ctx.body = await sequelize.query(query, options);
+  ctx.body = await Commit.findAll({
+    where: {
+      userId,
+    },
+    attributes: [[sequelize.fn('COUNT', sequelize.col('commit.id')), 'totalCommits']],
+    include: [{
+      model: Repo,
+      attributes: ['id', 'name', 'owner'],
+    }],
+    group: ['repoId'],
+  });
+
+  // const query = `
+  //   SELECT repoId, COUNT(commitId) AS commitsCount FROM users JOIN
+  //   (SELECT cㅏㅏㅓommits.id AS commitId, repos.id AS repoId, commits.userId AS userId
+  //   FROM commits JOIN repos ON commits.repoId = repos.id) AS bb 
+  //   ON users.id = bb.userId WHERE users.name = $username GROUP BY repoId;
+  // `;
+  // const options = {
+  //   type: sequelize.QueryTypes.SELECT,
+  //   bind: {
+  //     username,
+  //   },
+  // };
 });
 
 export default router;
