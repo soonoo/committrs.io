d3e277f75c1e03103cf12def412ab39f62fcd64e
soonoo <qpseh2m7@gmail.com>
2019-02-28 22:41:58 +0900

    [client] make UserRepoItem component to pass test

--- client/src/components/UserRepoItem/UserRepoItem.test.js
+++ client/src/components/UserRepoItem/UserRepoItem.test.js
@@ -53,9 +53,20 @@ describe('UserRepoItem.js', () => {
     for(const commit of props.commits) {
       const link = `https://github.com/${commit.repoPath}/commit/${commit.hash}`;
       expect(getByText(commit.hash).getAttribute('href')).toEqual(link);
-      expect(getByText(commit.message)).toBeDefined();
-      expect(getByText(commit.stat.addition.toString())).toBeDefined();
-      expect(getByText(commit.stat.deletion.toString())).toBeDefined();
+      expect(getByText(commit.message)).not.toBeNull();
+      expect(getByText(commit.stat.addition.toString())).not.toBeNull();
+      expect(getByText(commit.stat.deletion.toString())).not.toBeNull();
+    }
+  });
+
+  it('do not renders commits in repository when repo title is clicked twice', () => {
+    fireEvent.click(container.firstChild);
+
+    for(const commit of props.commits) {
+      expect(queryByText(commit.hash)).toBeNull();
+      expect(queryByText(commit.message)).toBeNull();
+      expect(queryByText(commit.stat.addition.toString())).toBeNull();
+      expect(queryByText(commit.stat.deletion.toString())).toBeNull();
     }
   });
 });
--- client/src/components/UserRepoItem/index.js
+++ client/src/components/UserRepoItem/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import GitCommitList from '../GitCommitList';
 
-const UserRepoItem = ({ owner, name, commitsCount }) => {
-  return (
-    <div>
-      <div>{`${owner}/${name}`}</div>
-      <div>{commitsCount}</div>
-    </div>
-  );
+class UserRepoItem extends React.Component {
+  state = {
+    commitListVisibility: false,
+  }
+
+  toggleCommitListVisibility = () => {
+    this.setState({
+      ...this.state,
+      commitListVisibility: !this.state.commitListVisibility,
+    });
+  }
+
+  render = () => {
+    const { owner, name, commitsCount, commits } = this.props;
+    const repoPath = `${owner}/${name}`;
+
+    return (
+      <div onClick={this.toggleCommitListVisibility}>
+        <div>{`${owner}/${name}`}</div>
+        <div>{commitsCount}</div>
+        {this.state.commitListVisibility &&
+            <GitCommitList commits={commits} repoPath={repoPath} />}
+      </div>
+    );
+  }
 };
 
 const { number, string } = PropTypes;
