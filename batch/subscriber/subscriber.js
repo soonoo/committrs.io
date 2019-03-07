e7a5ece42cfac5805ecfb6e5eb0e77648bdcc720
soonoo <qpseh2m7@gmail.com>
2019-03-07 21:12:10 +0900

    [batch] replace google pubsub to AWS SQS

--- batch/subscriber/subscriber.js
+++ batch/subscriber/subscriber.js
@@ -1,10 +1,27 @@
-import { PubSub } from '@google-cloud/pubsub';
+import AWS from 'aws-sdk';
 import handler from './handler';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const pubsub = new PubSub();
+const {
+  AWS_REGION: region,
+  AWS_ACCESS_KEY: accessKeyId,
+  AWS_SECRET_KEY: secretAccessKey,
+  AWS_SQS_QUEUE_URL: QueueUrl,
+} = process.env;
 
-const subscriptionName = 'committrs-sub';
-const subscription = pubsub.subscription(subscriptionName);
+const sqsConfig = {
+  region,
+  accessKeyId,
+  secretAccessKey,
+};
+const sqs = new AWS.SQS(sqsConfig);
 
-subscription.on(`message`, handler);
+sqs.receiveMessage(
+  { QueueUrl },
+  (err, data) => {
+    if(err) console.log(err);
+    if(data) console.log(data);
+  }
+);
 
