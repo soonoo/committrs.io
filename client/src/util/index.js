32d7d847f0f194782f25eb559780f72d531ee458
soonoo <qpseh2m7@gmail.com>
2019-03-09 05:39:40 -0500

    [client] fetch throws when response is not ok

--- client/src/util/index.js
+++ client/src/util/index.js
@@ -1,12 +1,20 @@
-const fetchWithErrorHandling = ({ url, method, options } = { method: 'GET', options: {} }) => {
+const fetchWithErrorHandling = ({ url, method = 'GET', options = {}}) => {
   return (
     fetch(url, {
       ...options,
       method,
     })
+    .then(response => {
+      if(!response.ok) throw new FetchResponseError(response);
+      return response;
+    })
     .then(response => response.json())
   );
 };
+
+const FetchResponseError = (response) => {
+  return new Error(`${response.status} ${response.statusText} - ${response.url}`);
+}
 
 export const GET = ({ url, options }) => fetchWithErrorHandling({ url, options });
 export const POST = ({ url, options }) => fetchWithErrorHandling({ url, options, method: 'POST' });
