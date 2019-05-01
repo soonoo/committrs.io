08a680c8a4a6b992f7120a02c68c48d7624c0ea0
soonoo <qpseh2m7@gmail.com>
2019-05-01 18:56:09 +0900

    [server] quit process when failed to create db

--- server/db/init.js
+++ server/db/init.js
@@ -3,6 +3,10 @@ const mysql = require('mysql');
 
 dotenv.config();
 
+const dbCreationFailed = () => {
+  process.exit(1);
+};
+
 const { DB_USERNAME: user, DB_PASSWORD: password, DB_NAME } = process.env;
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -10,7 +14,13 @@ const connection = mysql.createConnection({
   password,
 });
 
-connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME};`, (err) => {
-  if(!err) connection.end();
+console.log(`Creating database ${DB_NAME} ...`);
+connection.query(`CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;`, (err) => {
+  connection.end();
+
+  if(err) {
+    console.error(err)
+    dbCreationFailed();
+  }
 });
 
