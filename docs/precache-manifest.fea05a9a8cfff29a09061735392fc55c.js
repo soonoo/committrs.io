9839ed251a025700db58ea5d17049aa93dcf07e7
soonoo <qpseh2m7@gmail.com>
2019-05-20 22:46:59 +0900

    [client] test build for github page

---
+++ docs/precache-manifest.fea05a9a8cfff29a09061735392fc55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ff3562578b1bfdfacaf",
+    "url": "/soonoo/committrs/static/css/main.5c5a2114.chunk.css"
+  },
+  {
+    "revision": "2ff3562578b1bfdfacaf",
+    "url": "/soonoo/committrs/static/js/main.2ff35625.chunk.js"
+  },
+  {
+    "revision": "4bdc2deb0e369360337a",
+    "url": "/soonoo/committrs/static/js/1.4bdc2deb.chunk.js"
+  },
+  {
+    "revision": "97fce427a63867401714",
+    "url": "/soonoo/committrs/static/js/runtime~main.97fce427.js"
+  },
+  {
+    "revision": "bc97f0e90bac0ec3a86a2e72a8b26bd1",
+    "url": "/soonoo/committrs/index.html"
+  }
+];
