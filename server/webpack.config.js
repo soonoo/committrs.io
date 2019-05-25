7aad94665df1109806fa1df562467d4334e8afbc
soonoo <qpseh2m7@gmail.com>
2019-05-26 04:04:38 +0900

    [server] add argument for production build

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,38 +1,52 @@
 const path = require('path');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 
-module.exports = {
-  mode: 'development',
-  target: 'node',
-  node: {
-    __dirname: true,
-  },
-  entry: {
-    main: './src/index.js',
+module.exports = (env, argv) => {
+  const mode = argv.mode;
+  const plugins = [];
+  const entry = {
     test: './db/test.js',
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: '[name].bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/transform-runtime'],
+  };
+
+  if(mode === 'production') {
+    entry.app = './src/App.js';
+  } else {
+    entry.index = './src/index.js';
+    plugins.push(new WebpackShellPlugin({ onBuildStart: 'yarn dev' }));
+  }
+
+  return {
+    mode: mode || 'development',
+    target: 'node',
+    node: {
+      __dirname: true,
+    },
+    entry,
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: '[name].js',
+      libraryTarget: 'commonjs',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ['@babel/plugin-transform-async-to-generator'],
+            },
           },
         },
-      },
-    ],
-  },
-  externals: ['pg', 'sqlite3', 'tedious', 'pg-hstore'],
-  plugins: [
-    new WebpackShellPlugin({ onBuildStart: 'yarn dev' }),
-  ],
+      ],
+    },
+    externals: ['pg', 'sqlite3', 'tedious', 'pg-hstore'],
+    optimization: {
+      minimize: false,
+    },
+    plugins,
+  }
 };
 
