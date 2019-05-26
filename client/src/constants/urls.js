a51a237d902500cf91e82e5de18b6ecd8d10c31b
soonoo <qpseh2m7@gmail.com>
2019-05-26 22:51:31 +0900

    [client] change backend url for production

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,2 +1,2 @@
-export const SERVER_HOST = process.env.NODE_ENV === 'production' ? 'https://committrs.io' : 'http://localhost:8000';
+export const SERVER_HOST = process.env.NODE_ENV === 'production' ? 'https://api.committrs.io' : 'http://localhost:8000';
 
