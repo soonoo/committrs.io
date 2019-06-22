276f89bc4b11620ec5ce853648aa0e9cb6e7e1b3
soonoo <qpseh2m7@gmail.com>
2019-06-22 22:21:15 +0900

    [server] change node-fetch to axios

--- server/src/routes/authController.js
+++ server/src/routes/authController.js
@@ -1,37 +1,35 @@
 import User from '../../db/model/User';
 import Router from 'koa-router';
 import jwt from 'jsonwebtoken';
-import { POST, GET } from '../util';
+import axios from 'axios';
 
+const isProduction = process.env.NODE_ENV === 'production';
 const router = new Router();
 
 router.get('/github/token', async (ctx) => {
+  const githubLoginUrl = 'https://github.com/login/oauth/access_token';
+  const githubTokenUrl = 'https://api.github.com/user';
+  const postfix = isProduction ? '' : '_DEV';
+  const client_id = process.env[`GITHUB_ID${postfix}`];
+  const client_secret = process.env[`GITHUB_SECRET${postfix}`];
+  const client_host = process.env[`CLIENT_HOST${postfix}`];
+  const { JWT_SECRET } = process.env;
   const { code } = ctx.query;
-  const { GITHUB_ID: client_id, GITHUB_SECRET: client_secret, JWT_SECRET } = process.env;
-  let url = `https://github.com/login/oauth/access_token`;
   let options = {
-    body: JSON.stringify({
-      client_id,
-      client_secret,
-      code,
-    }),
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-  }
-  const { access_token } = await POST({ url, options });
-
-  url = `https://api.github.com/user?access_token=${access_token}`;
-  options = {
-    headers: {
-      'Accept': 'application/json'
     },
   };
-  const { avatar_url, login, email } = await GET({ url, options });
+  const body = {
+    client_id,
+    client_secret,
+    code,
+  };
+
+  const { data: { access_token } } = await axios.post(githubLoginUrl, body, options);
+  const { data: { avatar_url, login, email } } = await axios.get(`${githubTokenUrl}?access_token=${access_token}`);
   let user = await User.upsert({
     name: login,
-    email,
     token: access_token,
     email,
     avatarUrl: avatar_url,
@@ -39,7 +37,7 @@ router.get('/github/token', async (ctx) => {
 
   const token = jwt.sign({ name: login, avatarUrl: avatar_url }, JWT_SECRET);
   ctx.cookies.set('cmtrs-token', token);
-  ctx.redirect(`/${login}`)
+  ctx.redirect(`${client_host}/${login}`);
 });
 
 export default router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,13 +5,11 @@ import repoController from './repoController';
 import userController from './userController';
 
 const router = new Router();
+router.prefix('/v1');
 router.use('/auth', authController.routes());
 router.use('/commits', commitController.routes());
 router.use('/repos', repoController.routes());
 router.use('/users', userController.routes());
 
-const v1 = new Router();
-v1.use('/v1', router.routes());
-
-export default v1;
+export default router;
 
