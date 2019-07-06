9f276982801d55187920c06f3926bfcaf2a1e535
soonoo <qpseh2m7@gmail.com>
2019-07-07 03:38:03 +0900

    [client] add login link

--- client/src/components/GithubLoginButton/index.js
+++ client/src/components/GithubLoginButton/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import GithubLogo from 'images/github-logo.svg';
+import { SERVER_HOST, GITHUB_ID } from 'constants/index';
 
 const GithubLoginButton = () => {
+  const url = `https://github.com/login/oauth/authorize?client_id=${GITHUB_ID}&redirect_uri=${SERVER_HOST}/v1/auth/github/token`;
+  const login = () => {
+    window.location.href = url;
+  }
+
   return (
-    <div className='github-login'>
-      <img className='logo' src={GithubLogo} />
+    <div onClick={login} className='github-login'>
+      <img className='logo' src={GithubLogo} alt='octocat' />
       <span className='desc'>Continue with GitHub</span>
     </div>
   );
