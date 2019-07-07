2e2fbc6913c52e3f9474d4a7b9a81b208a680246
soonoo <qpseh2m7@gmail.com>
2019-07-07 22:06:31 +0900

    [client] add some style to landing page

--- client/src/components/MainPagePhrase/index.js
+++ client/src/components/MainPagePhrase/index.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 const MainPagePhrase = () => {
   return (
     <Fragment>
-      <h1 className='title'>Collect your contributions to open source projects.</h1>
-      <div className='title-sub'>
-        <h2>
+      <h1 className='title'><b>committrs.io</b></h1>
+      <div className='title-sub-wrapper'>
+        <h2 className='title-sub'>
           What is commitrrs?
         </h2>
-        <span>
+        <span className='title-sub-desc'>
           People contibute to various open source projects. 
           However it is difficult to see actual contributions in one place. committrs finds your contributions scattered over open source projects and provides a profile page.
         </span>
       </div>
-      <div className='title-sub'>
-        <h2>
+      <div className='title-sub-wrapper'>
+        <h2 className='title-sub'>
           What platform does committrs support?
         </h2>
-        <span>
+        <span className='title-sub-desc'>
           committrs.io now only collects your commits in GitHub repository with over 30 stars. 
-          Support for other platforms(e.g. Gitlab) will be added in the future.
+            Support for other platforms(e.g. Gitlab, MDN) will be added in the future.
         </span>
       </div>
       <span className='sample-link'>
