51e6735eb3b6ffc2bd0f3c4a0678d7c3adcc049e
soonoo <qpseh2m7@gmail.com>
2019-08-18 01:06:04 +0900

    [batch] fix api server host name

--- batch/constants/index.js
+++ batch/constants/index.js
@@ -5,7 +5,7 @@ const delimiters = [
   '---committrs/body---\n',
   '---committrs/files_changed---\n'
 ];
-const BACKEND_HOST = process.env.NODE_ENV === 'production' ? 'https://committrs.io' : 'http://localhost:8000';
+const BACKEND_HOST = process.env.NODE_ENV === 'production' ? 'https://api.committrs.io' : 'http://localhost:8000';
 
 export { delimiters, BACKEND_HOST };
 
