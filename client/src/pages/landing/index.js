12fb886cd35e88400b86ba198ba99c560b9a8b1c
soonoo <qpseh2m7@gmail.com>
2019-08-18 18:24:36 +0900

    [client] set title of landing page with react-helmet

--- client/src/pages/landing/index.js
+++ client/src/pages/landing/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import MainPagePhrase from 'components/MainPagePhrase';
 import GithubLoginButton from 'components/GithubLoginButton';
 import Header from 'components/Header';
+import { Helmet } from 'react-helmet';
 
 import './landing.css';
 
 const LandingPage = () => {
   return (
     <div>
+      <Helmet>
+        <title>committrs.io - Get your profile page for open source contrbutions.</title>
+      </Helmet>
       <Header />
       <div className='landing'>
         <div className='wrapper'>
