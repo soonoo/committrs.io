df55909820e38f7c2c794434a185b5937b4f4d3c
soonoo <qpseh2m7@gmail.com>
2019-10-06 01:59:24 +0900

    [batch] fix repository validation logic

--- batch/subscriber/handler/newUserHandler.js
+++ batch/subscriber/handler/newUserHandler.js
@@ -22,6 +22,7 @@ const getUserRepos = async (username) => {
     try {
       tempRepoList = await octokit.repos.listForUser({ username, per_page, page: page++ })
       repoData = repoData.concat(tempRepoList.data);
+      console.log('user repos: ', repoData.length)
     } catch(e) {
       console.error(e);
     }
@@ -32,6 +33,7 @@ const getUserRepos = async (username) => {
     try {
       tempOrgsList = await octokit.orgs.listForUser({ username, per_page, page: page++ })
       orgsData = orgsData.concat(tempOrgsList.data);
+      console.log('orgs: ', orgsData.length)
     } catch(e) {
       console.error(e);
     }
@@ -43,13 +45,11 @@ const getUserRepos = async (username) => {
     do {
       rr = await octokit.repos.listForOrg({ org: org.login, per_page, page: page++ });
       repoData = repoData.concat(rr.data);
+      console.log('user repos: ', repoData.length)
     } while(rr.data.length === 100);
   }
 
-  for(const n of repoData) {
-    console.log(n.full_name)
-  }
-
+  console.log(repoData.length)
   return repoData;
 }
 
@@ -57,11 +57,13 @@ const getValidRepos = async (repos, minimumStarCount) => {
   let i = 0;
   const repoList = [];
 
+  console.log('callint getValidRepos with count: ', repos.length)
   for(const repo of repos) {
+    console.log('checking ' + repo.owner + '/'+  repo.name)
     try {
       if(repo.stargazers_count >= minimumStarCount) {
-        const rr = await octokit.repos.get({ owner: repo.owner.login, repo: repo.name });
-        if((rr.data.size / 1024) > maxRepoSizeInMegaBytes) {
+        if((repo.size / 1024) > maxRepoSizeInMegaBytes) {
+          console.log(repo.owner + '/'+  repo.name + ' too big')
           continue;
         }
         repoList.push({ owner: repo.owner.login, name: repo.name, starsCount: repo.stargazers_count, description: repo.description });
@@ -70,6 +72,7 @@ const getValidRepos = async (repos, minimumStarCount) => {
         const r = await octokit.repos.get({ owner: repo.owner.login, repo: repo.name });
         const rrr = await octokit.repos.get({ owner: r.data.parent.owner.login, repo: r.data.parent.name });
         if((rrr.data.size / 1024) > maxRepoSizeInMegaBytes) {
+          console.log(repo.data.owner + '/' + rrr.data.name + ' too big')
           continue;
         }
         if(r.data.parent.stargazers_count >= minimumStarCount) {
@@ -81,6 +84,7 @@ const getValidRepos = async (repos, minimumStarCount) => {
     }
   }
 
+  console.log(repoList.length)
   return repoList;
 }
 
@@ -112,8 +116,10 @@ const newUserHandler = async ({ message, token }) => {
   const userRepos = await getUserRepos(github_login);
   let validRepos = await getValidRepos(userRepos, minimumStarCount);
   validRepos = validRepos.filter((repo, i) => {
-    return validRepos.findIndex(r => r.owner === repo.owner && r.name === r.name) === i;
+    return validRepos.findIndex(r => r.owner === repo.owner && r.name === repo.name) === i;
   });
+  console.log('un-filtered validRepos lenght: ' + validRepos.length)
+  console.log('filtered validRepos lenght: ' + validRepos.length)
 
   const { data: { id: userId } } = await instance.get(`/v1/users/${github_login}`);
 
@@ -127,10 +133,12 @@ const newUserHandler = async ({ message, token }) => {
 
       const path = `${repo.owner}/${repo.name}`;
       const fullPath = `${cwd}/repos/${path}`;
+      console.log('cloning ' + repo.owner + '/' + repo.name);
       await refresh(cwd, path);
 
       const gitLog = await log([github_login, github_name].join('|'), fullPath);
       const commits = splitCommits(gitLog);
+      console.log('commits count' + commits.length);
 
       for(const bulk of _.chunk(commits, 500)) {
         await instance.put(`/v1/commits/bulk/${userId}/${repoId}`, bulk);
